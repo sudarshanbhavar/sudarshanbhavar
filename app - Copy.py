542839be823c9593b08ee85b6a92b51739a60384
Sudarshan Bhavar <105584254+sudarshanbhavar@users.noreply.github.com>
2023-05-23 23:33:45 +0530

    Update app - Copy.py

--- app - Copy.py
+++ app - Copy.py
@@ -57,11 +57,11 @@ def upload():
         print(preds[0])
 
         # x = x.reshape([64, 64]);
-        disease_class = ['Pepper__bell___Bacterial_spot', 'Pepper__bell___healthy', 'Potato___Early_blight',
-                         'Potato___Late_blight', 'Potato___healthy', 'Tomato_Bacterial_spot', 'Tomato_Early_blight',
-                         'Tomato_Late_blight', 'Tomato_Leaf_Mold', 'Tomato_Septoria_leaf_spot',
-                         'Tomato_Spider_mites_Two_spotted_spider_mite', 'Tomato__Target_Spot',
-                         'Tomato__Tomato_YellowLeaf__Curl_Virus', 'Tomato__Tomato_mosaic_virus', 'Tomato_healthy']
+        disease_class = ['Bacterial_spot', '_healthy', 'Early_blight',
+                         'Late_blight', 'healthy', 'Bacterial_spot', 'Early_blight',
+                         'Late_blight', 'Leaf_Mold', 'leaf_spot',
+                         'mite', 'Target_Spot',
+                         'YellowLeaf__Curl_Virus', 'mosaic_virus', 'healthy']
         a = preds[0]
         ind=np.argmax(a)
         print('Prediction:', disease_class[ind])
